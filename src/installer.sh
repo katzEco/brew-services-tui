37f19c5e6d74b2c3a4b708d447a46ba299a92e5b
Suphakit P <me@suphakit.net>
2023-06-27 17:22:25 +0700

    feat: setting up installer

--- src/installer.sh
+++ src/installer.sh
@@ -1,1 +1,21 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
+
+read -p "Do you want to install this package? [y/N] : " option
+
+if [[ $option =~ ^[y]+$ || $option =~ ^[Y]+$ ]]; then
+  mkdir ~/.config/.bst
+  cd ~/.config/.bst
+  git clone https://github.com/katzEco/brew-services-tui
+  cd brew-services-tui/src
+  mv * ~/.config/.bst
+  cd ~/.config/.bst
+  rm -rf brew-services-tui
+  rm installer.sh
+
+  echo alias bst="~/.config/.bst/tui.sh" > ~/.zshrc
+  source ~/.zshrc
+  clear
+  
+  echo "bst is installed to your console"
+  echo "try run 'bst' in your zsh"
+fi
