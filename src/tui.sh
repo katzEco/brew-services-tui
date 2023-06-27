5b130c94025d7bdf5cb28532fcca7bb1d27f73f5
Suphakit P <me@suphakit.net>
2023-06-27 15:46:28 +0700

    chore<tui>: command list

--- src/tui.sh
+++ src/tui.sh
@@ -36,7 +36,7 @@ clear
 brewServices() {
   # brew services list | grep yabai | awk '{ print $2}'
   echo "==================================================="
-  echo "          ${bold}${underline}Homebrew Services Configuration${normal}"
+  echo "           ${bold}${underline}Homebrew Services Status List${normal}"
   echo ""
   
   if [[ $yabaiStatus =~ ^[1]+$ ]]; then 
@@ -53,6 +53,166 @@ brewServices() {
 
   echo ""
   echo "==================================================="
+}
+
+yabaiServices() {
+  while [[ ! $yabaiOption =~ ^[0]+$ ]]; do
+    echo "==================================================="
+    echo "                ${bold}${underline}Yabai Configuration${normal}"
+    echo "Yabai Status: $(brew services list | grep yabai | awk '{ print $2}')"
+
+    if [[brew services list | grep yabai | awk '{ print $2}' == 'started']]; then
+      echo "1. Service start"
+      echo "${bold}2. Service stop"
+    else
+      echo "${bold}1. Service start"
+      echo "2. Service stop"
+    fi
+
+    echo "3. Services Restart"
+    echo "0. Return to Home"
+
+    echo ""
+    echo "==================================================="
+
+    read -p "Enter your option : " yabaiOption
+
+    if [[ $yabaiOption =~ ^[1]+$ ]]; then
+      clear
+      echo "==================================================="
+      echo ""
+      brew services start yabai
+      echo ""
+      echo "==================================================="
+      sleep 3 &
+      wait
+      clear
+      echo "==================================================="
+      echo ""
+      echo "Yabai Services Started"
+      echo ""
+      echo "==================================================="
+    elif [[ $yabaiOption =~ ^[2]+$ ]]; then
+      clear
+      echo "==================================================="
+      echo ""
+      brew services stop yabai
+      echo ""
+      echo "==================================================="
+      sleep 3 &
+      wait
+      clear
+      echo "==================================================="
+      echo ""
+      echo "Yabai Services Stopped"
+      echo ""
+      echo "==================================================="
+    elif [[ $yabaiOption =~ ^[3]+$ ]]; then
+      clear
+      echo "==================================================="
+      echo ""
+      brew services stop yabai
+      brew services start yabai
+      echo ""
+      echo "==================================================="
+      sleep 3 &
+      wait
+      clear
+      echo "==================================================="
+      echo ""
+      echo "Yabai Services Restarted"
+      echo ""
+      echo "==================================================="
+    elif [[ $yabaiOption =~ ^[0]+$ ]]; then
+      clear
+    else
+      clear
+      echo 'Unknown option!'
+      sleep 5 &
+      wait
+      clear
+    fi
+  done
+}
+
+skbServices() {
+  while [[ ! $skbOption =~ ^[0]+$ ]]; do
+    echo "==================================================="
+    echo "             ${bold}${underline}SketchyBar  Configuration${normal}"
+    echo "Yabai Status: $(brew services list | grep yabai | awk '{ print $2}')"
+
+    if [[brew services list | grep sketchybar | awk '{ print $2}' == 'started']]; then
+      echo "1. Service start"
+      echo "${bold}2. Service stop"
+    else
+      echo "${bold}1. Service start"
+      echo "2. Service stop"
+    fi
+
+    echo "3. Services Restart"
+    echo "0. Return to Home"
+
+    echo ""
+    echo "==================================================="
+
+    read -p "Enter your option : " skbOption
+
+    if [[ $skbOption =~ ^[1]+$ ]]; then
+      clear
+      echo "==================================================="
+      echo ""
+      brew services start sketchybar
+      echo ""
+      echo "==================================================="
+      sleep 3 &
+      wait
+      clear
+      echo "==================================================="
+      echo ""
+      echo "SketchyBar Services Started"
+      echo ""
+      echo "==================================================="
+    elif [[ $skbOption =~ ^[2]+$ ]]; then
+      clear
+      echo "==================================================="
+      echo ""
+      brew services stop sketchybar
+      echo ""
+      echo "==================================================="
+      sleep 3 &
+      wait
+      clear
+      echo "==================================================="
+      echo ""
+      echo "SketchyBar Services Stopped"
+      echo ""
+      echo "==================================================="
+    elif [[ $skbOption =~ ^[3]+$ ]]; then
+      clear
+      echo "==================================================="
+      echo ""
+      brew services stop sketchybar
+      brew services start sketchybar
+      echo ""
+      echo "==================================================="
+      sleep 3 &
+      wait
+      clear
+      echo "==================================================="
+      echo ""
+      echo "SketchyBar Services Restarted"
+      echo ""
+      echo "==================================================="
+    elif [[ $skbOption =~ ^[0]+$ ]]; then
+      clear
+    else
+      clear
+      echo 'Unknown option!'
+      sleep 5 &
+      wait
+      clear
+    fi
+  done
 }
 
 while [[ ! $options =~ ^[4]+$ ]]; do
@@ -82,12 +242,14 @@ while [[ ! $options =~ ^[4]+$ ]]; do
   echo ""
   echo "==================================================="
 
-  read -p "Enter Your Config : " options
+  read -p "Enter your option : " options
 
   if [[ $options =~ ^[1]+$ ]]; then
-    echo '1'
+    clear
+    echo "$(yabaiServices)"
   elif [[ $options =~ ^[2]+$ ]]; then
-    echo '2'
+    clear
+    echo "$(skbServices)"
   elif [[ $options =~ ^[3]+$ ]]; then
     clear
     echo "$(brewServices)"
@@ -95,11 +257,11 @@ while [[ ! $options =~ ^[4]+$ ]]; do
     echo ''
     clear
   else
-    # clear
+    clear
     echo 'Unknown option!'
     sleep 5 &
     wait
-    # clear
+    clear
   fi
 done
 
